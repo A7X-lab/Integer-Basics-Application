35fa71b99ad2ae6e8ce189293e6b2783fad7f821
A7X-lab <134360870+A7X-lab@users.noreply.github.com>
2023-08-08 18:37:27 -0400

    Add files via upload

---
+++ Basic Input Application.py
@@ -0,0 +1,20 @@
+#============================================================================
+# Name        : Python Basic Project
+# Author      : Kirklen Allen
+# Version     : 1.0
+# Description : IT 140  Python Project
+#============================================================================
+
+
+Person_Name = input()
+Person_Age = int(input())
+Year = int(input())
+
+# Prompt user to input age
+print ('What is your name?', Person_Name )
+
+# Prompt user for age
+print ('How Old Are You?', Person_Age)
+
+#Output Statement with User input
+print ('Hello' , Person_Name,'!', 'You were born in', Year,'.')
